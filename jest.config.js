cc1c3b7a8b5887c1d8727d96394197047828f5e2
Andrés Mendoza Villalba <93752506+AndresM412@users.noreply.github.com>
2025-03-08 16:51:43 -0500

    fix jest config
    habia problemas de configuracion de jest, se asigna configuraciones de jest al archivo "jest.config.js"

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    testTimeout: 10000, // Aumenta el timeout global a 10000 ms (10 segundos)
-  };
+  testTimeout: 10000, // Aumenta el timeout global a 10000 ms (10 segundos)
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+  testEnvironment: "node",
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/server.js",  // Excluye el archivo server.js si no deseas medir su cobertura
+    "!tests/**/*.js"   // Excluye los archivos de prueba
+  ]
+};
