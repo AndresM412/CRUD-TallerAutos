5811e06b51becd5ad82b048a8b8222b0f63f07cd
Andrés Mendoza Villalba <93752506+AndresM412@users.noreply.github.com>
2025-03-12 14:31:48 -0500

    Forzar la Transformación de Todas las Dependencias con babel

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,15 @@
 module.exports = {
-  testTimeout: 10000, // Aumenta el timeout global a 10000 ms (10 segundos)
+  testTimeout: 10000,
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov"],
   testEnvironment: "node",
   collectCoverageFrom: [
     "src/**/*.js",
-    "!src/server.js",  // Excluye el archivo server.js si no deseas medir su cobertura
-    "!tests/**/*.js"   // Excluye los archivos de prueba
+    "!src/server.js",
+    "!tests/**/*.js"
   ],
   transform: {
-    "^.+\\.js$": "babel-jest" // Usa Babel para transformar archivos .js
-  },
-  transformIgnorePatterns: [
-    "/node_modules/(?!bson).+\\.js$"
-  ]
+    "^.+\\.js$": "babel-jest"
+  }
 };
