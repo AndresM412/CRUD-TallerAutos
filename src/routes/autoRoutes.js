e21a8e302ef30b334be9a4007f0cf8cecb6aab0b
Andrés Mendoza Villalba <93752506+AndresM412@users.noreply.github.com>
2025-04-02 12:34:46 -0500

    fix: corregir nombre de función getAuto a getAllAutos en rutas

--- src/routes/autoRoutes.js
+++ src/routes/autoRoutes.js
@@ -4,7 +4,7 @@ const autoController = require("../controllers/autoController");
 const router = express.Router();
 
 router.post("/", autoController.createAuto);
-router.get("/", autoController.getAuto);
+router.get("/", autoController.getAllAutos);
 router.put("/:id", autoController.updateAuto);
 router.delete("/:id", autoController.deleteAuto);
 
